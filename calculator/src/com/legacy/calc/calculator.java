8566199905502325a7acd31693917965d848b904
Ajas Shaikh <Ajas Shaikh@DESKTOP-BDOFJL8>
2019-01-19 12:44:51 +0530

    main code

---
+++ calculator/src/com/legacy/calc/calculator.java
@@ -0,0 +1,18 @@
+package com.legacy.calc;
+
+public class calculator {
+
+	
+	
+	public long add(int a , int b) {
+		
+		
+		return (a+b);
+			
+	}
+	
+	public int sub(int a , int b) {
+		
+		return (a-b);
+	}
+}
